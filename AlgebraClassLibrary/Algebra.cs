a54966f4801b4ffb4810882e86f83ef34c03fc6d
yhlica <yli202@my.centennialcollege.ca>
2023-03-30 17:49:51 -0400

    Add project files.

---
+++ AlgebraClassLibrary/Algebra.cs
@@ -0,0 +1,18 @@
+﻿namespace AlgebraClassLibrary
+{
+    public class Algebra
+    {
+        public double Addition(double x, double y)
+        { return x + y; }
+
+        public double Subtraction(double x, double y)
+        { return x - y; }
+
+        public double Multiplication(double x, double y)
+        { return x * y; }
+
+        public double Division(double x, double y)
+        { return x / y; }
+    }
+
+}
